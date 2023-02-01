63ce869823fe8a94f04914090e301001138c87a8
icecube-pixel <nithinbharadwajd@outlook.com>
2023-02-01 01:59:57 -0500

    Added models utils and github workflow

---
+++ models/users.js
@@ -0,0 +1,39 @@
+const {sequelize, DataTypes } = require('./../dataUtils');
+
+const User = sequelize.define('users', {
+    
+    firstName: {
+        field : 'first_name',
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    lastName: {
+        field : 'last_name',
+        type: DataTypes.STRING,
+        allowNull : false
+    },
+    password : {
+        field : 'password',
+        type: DataTypes.STRING,
+        allowNull : false
+    },
+    userName : {
+        field : 'username',
+        type: DataTypes.STRING,
+        allowNull : false
+    },
+
+    account_created : {
+        type: DataTypes.NOW
+    },
+
+    account_updated : {
+        type: DataTypes.NOW
+    }
+
+  }, {
+    timestamps : false
+});
+
+module.exports = User;
+  
