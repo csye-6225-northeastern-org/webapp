5d2d4af11895ec013e01c672d0b4535d6a2f0c76
icecube-pixel <nithinbharadwajd@outlook.com>
2023-02-01 02:08:59 -0500

    Added tests app.js dataUtils.js and package.json

---
+++ app.test.js
@@ -0,0 +1,55 @@
+const request = require('supertest');
+const app = require('./app');
+const validations = require('./utils/validations');
+
+describe('Test the GET /v1/healthz endpoint', () => {
+    it('should return 200 OK', async () => {
+      const response = await request(app).get('/v1/healthz');
+      expect(response.statusCode).toBe(200);
+    });
+  });
+
+describe('Test the email validation function', () => {
+  it('should return true if valid email is passed to function', () => {
+    const validationResult = validations.validateEmail("nithinbharadwajd@gmail.com");
+    expect(validationResult).toBe(true);
+  });
+
+  it('should return false if in-valid email is passed to function', () => {
+    const validationResult = validations.validateEmail("asdasdasd.asdsd@@@@gmail.com");
+    expect(validationResult).toBe(false);
+  });
+
+});
+
+ /*
+
+describe("GET /v1/user/:userId", () => {
+    it("should have a userId in the request parameters and authorization in header", async () => {
+      const userId = "3";
+      const username = "kiran@gmail.com";
+      const password = "kiran";
+      const encodedCredentials = Buffer.from(`${username}:${password}`).toString("base64");
+      const res = await request(app).get(`/v1/user/${userId}`)
+      .set("Authorization", `Basic ${encodedCredentials}`)
+      .expect(200);      
+    });
+  });
+  
+describe("POST /v1/user", () => {  
+    it("returns 400 Bad Request if username already exists", async () => {
+      const user = {
+        first_name: "kiran",
+        last_name: "reddy",
+        username: "kiran@example.com",
+        password: "kiran"
+      };
+  
+      await request(app)
+        .post("/v1/user")
+        .send(user)
+        .expect(400);
+    });
+  });
+  
+  */
