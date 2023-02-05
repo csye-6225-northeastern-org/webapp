5c5f71fe1eb6a95d056f766a8ffe05b4be81f3e3
icecube-pixel <nithinbharadwajd@outlook.com>
2023-02-05 15:47:28 -0500

    Added changes in the config and db code

--- postgres/db.js
+++ postgres/db.js
@@ -2,12 +2,15 @@ const { Sequelize, DataTypes} = require('sequelize');
 const { dbName,
         hostUrl, 
         userName,
-        password} = require("./config")
+        password,
+        dbPoolConfig} = require("./config")
 
-const sequelize = new Sequelize(dbName, userName, password, {
-                                    host: hostUrl,
-                                    dialect: "postgres"
-                                });
+const sequelize = new Sequelize(dbName, userName, password, 
+        {
+            host: hostUrl,
+            dialect: "postgres",
+            pool : dbPoolConfig
+        });
 
 module.exports = {
     sequelize,
