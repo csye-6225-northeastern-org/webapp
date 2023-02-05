c8e0393fb83427a92f339d412c68ab68aebfc46e
icecube-pixel <nithinbharadwajd@outlook.com>
2023-02-05 15:47:28 -0500

    Added changes in the config and db code

--- postgres/config.js
+++ postgres/config.js
@@ -1,9 +1,18 @@
 require('dotenv').config();
 
+const dbPoolConfig = {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+};
+
+
 module.exports = {
     dialect : process.env.DIALECT,
     dbName : process.env.DB_NAME,
     hostUrl : process.env.HOST,
     userName : process.env.USERNAME,
-    password : process.env.PASSWORD
+    password : process.env.PASSWORD,
+    dbPoolConfig
 }
