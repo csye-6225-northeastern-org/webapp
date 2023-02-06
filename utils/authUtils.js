168db8e2419e984cc756e345becb0a304853d395
icecube-pixel <nithinbharadwajd@outlook.com>
2023-02-05 19:32:09 -0500

    Added changes in the PUT block of the APIs

--- utils/authUtils.js
+++ utils/authUtils.js
@@ -6,6 +6,11 @@ async function comparePassword(enteredPassword, hash){
     return await bcrypt.compare(enteredPassword, hash);
 }
 
+async function generateHash(userPassword){
+    return await bcrypt.hash(userPassword, saltRounds);
+}
+
 module.exports = {
-    comparePassword
+    comparePassword,
+    generateHash
 };
