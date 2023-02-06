4b23e14c0aaac4358862559cb3ea48652da48cef
icecube-pixel <nithinbharadwajd@outlook.com>
2023-02-05 21:36:02 -0500

    Added boiler plate code across routes, models and controllers

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ const express = require('express')
 const app = express()
 
 app.use(express.json())
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 const index_routes = require('./routes/index');
 const userRoutes = require('./routes/users');
+const productRoutes = require('./routes/product');
 
 app.use('/', index_routes);
 app.use('/v1/user', userRoutes);
+app.use('/v1/product', productRoutes);
 
 module.exports = app;
