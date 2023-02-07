e455fefa799401114476240cc86cb8a065678a64
icecube-pixel <nithinbharadwajd@outlook.com>
2023-02-07 16:47:23 -0500

    Added changes to models and config to take the node env as variable

--- controllers/product.js
+++ controllers/product.js
@@ -1,11 +1,24 @@
 const product = require('../models/products');
+const validations = require('../utils/validations');
+const ProductService = require('../services/productService');
+
+let productService = new ProductService(); 
 
 exports.getProductInfo = ((req, res) => {
 
 });
 
 exports.postProductInfo = ((req, res) => {
+    const {name, description, sku, manufacturer, quantity, date_added, date_last_updated, owner_user_id} = req.body;
+    if(date_added || date_last_updated || owner_user_id){
+        res.status(400).send({"message" : "400 Bad Request. Cannot send date_added / date_last_updated /owner_user_id"});
+    }else if(validations.validateQuantity(quantity)){
+        res.status(400).send({"message" : "400 Bad Request. Invalid Quantity Sent in the payload"});
+    }else{
+        
+    }
 
+    // productService
 });
 
 exports.updateProductInfo = ((req, res) => {
---
+++ services/productService.js
@@ -0,0 +1,23 @@
+const Product = require("../models/products");
+class ProductService {
+    
+    async createProduct(product){
+        return await Product.create(product);
+    }
+
+    async findOne(productId){
+        return await Product.findOne({where: {id: productId}});
+    }
+
+    async updateProduct(id, payload) {
+        return await User.update(payload, {
+          where: {
+            id
+          }
+        });
+      }
+    
+
+}
+
+module.exports = ProductService;
