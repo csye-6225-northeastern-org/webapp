694482b1c69537cfcc4c12aadd251b1a6b467fa7
icecube-pixel <nithinbharadwajd@outlook.com>
2023-02-08 02:55:17 -0500

    Added functionality for pUT and pATCH

--- controllers/product.js
+++ controllers/product.js
@@ -127,12 +127,33 @@ exports.putProductInfo = ((req, res) => {
                     passCompare.then(cmpResult => {
                         if(cmpResult){
                             const userIdAccessing = result.getDataValue("id");
-                            productService.findOneBySku(sku)
-                            .then(rowFound=>{
-                                if(rowFound){
-                                    res.status(400).send({"message" : "400 Bad Request - SKU Already exists"})
-                                }else{
-                                    productService.findOne(id)
+                            if(sku){
+                                productService.findOneBySku(sku)
+                                .then(rowFound=>{
+                                    if(rowFound){
+                                        res.status(400).send({"message" : "400 Bad Request - SKU Already exists"})
+                                    }else{
+                                        productService.findOne(id)
+                                        .then(productRow => {
+                                            if(!productRow){
+                                                res.status(400).send({"message" : "400 Not Found"});
+                                            }else if(productRow.dataValues.owner_user_id !==  userIdAccessing){
+                                                res.status(403).send({"message" : "403 Forbidden - Not allowed to delete"});
+                                            }else{
+                                                productService.updateProductInfo({name, description,
+                                                            sku, manufacturer, quantity}, id)
+                                                .then( updatedRow => {
+                                                    if(updatedRow){
+                                                        res.status(204).send({});  
+                                                    }
+                                                })
+                                                
+                                            }
+                                        })
+                                    }
+                                })
+                            }else{
+                                productService.findOne(id)
                                     .then(productRow => {
                                         if(!productRow){
                                             res.status(400).send({"message" : "400 Not Found"});
@@ -142,14 +163,14 @@ exports.putProductInfo = ((req, res) => {
                                             productService.updateProductInfo({name, description,
                                                         sku, manufacturer, quantity}, id)
                                             .then( updatedRow => {
-                                                console.log("Updated Row info : ", updatedRow);
+                                                if(updatedRow){
+                                                    res.status(204).send({});  
+                                                }
                                             })
                                             
                                         }
                                     })
-                                }
-                            })
-                            
+                            }
                         }else{
                             res.status(401).send({"message" : "401 Unauthorized"});
                         }
@@ -162,6 +183,86 @@ exports.putProductInfo = ((req, res) => {
 });
 
 exports.patchProductInfo = ((req, res) => {
+
+    const { id } = req.params;
+    const {name, description, sku, manufacturer, quantity, date_added, date_last_updated, owner_user_id} = req.body;
+    const inputCheckBool = checkIdInput(req, res);
+    if(inputCheckBool){
+        res.status(400).send({"message" : "400 Bad Request - Invalid Id in the request"});
+    }else if(date_added || date_last_updated || owner_user_id){
+        res.status(400).send({"message" : "400 Bad Request - Cannot send date_added / date_last_updated /owner_user_id"});
+    }else if(validations.validateQuantity(quantity)){
+        res.status(400).send({"message" : "400 Bad Request - Invalid Quantity Sent in the payload"});
+    }else{
+        const credentials = checkAuthHeaders(req, res);
+        if(utils.isObjEmpty(credentials)){
+            res.status(401).send({"message" : "Unauthorized - No Authorization found in headers"});
+        }else if(credentials.username==='' || credentials.password === ''){
+            res.status(401).send({"message" : "Unauthorized - Missing username/password"});
+        }else{
+            userService.findOneByUsername(credentials.username)
+            .then( result =>{
+                if(!result){
+                    res.status(401).send({"message" : "401 Unauthorized. No user found"});
+                }else if(result.dataValues.userName !== credentials.username){
+                    res.status(403).send({"message" : "403 Forbidden"});
+                }else{
+                    const passCompare = authUtils.comparePassword(credentials.password, result.dataValues.password);
+                    passCompare.then(cmpResult => {
+                        if(cmpResult){
+                            const userIdAccessing = result.getDataValue("id");
+                            if(sku){
+                                productService.findOneBySku(sku)
+                                .then(rowFound=>{
+                                    if(rowFound){
+                                        res.status(400).send({"message" : "400 Bad Request - SKU Already exists"})
+                                    }else{
+                                        productService.findOne(id)
+                                        .then(productRow => {
+                                            if(!productRow){
+                                                res.status(400).send({"message" : "400 Not Found"});
+                                            }else if(productRow.dataValues.owner_user_id !==  userIdAccessing){
+                                                res.status(403).send({"message" : "403 Forbidden - Not allowed to delete"});
+                                            }else{
+                                                productService.updateProductInfo({name, description,
+                                                            sku, manufacturer, quantity}, id)
+                                                .then( updatedRow => {
+                                                    if(updatedRow){
+                                                        res.status(204).send({});  
+                                                    }
+                                                })
+                                                
+                                            }
+                                        })
+                                    }
+                                })
+                            }else{
+                                productService.findOne(id)
+                                    .then(productRow => {
+                                        if(!productRow){
+                                            res.status(400).send({"message" : "400 Not Found"});
+                                        }else if(productRow.dataValues.owner_user_id !==  userIdAccessing){
+                                            res.status(403).send({"message" : "403 Forbidden - Not allowed to delete"});
+                                        }else{
+                                            productService.updateProductInfo({name, description,
+                                                        sku, manufacturer, quantity}, id)
+                                            .then( updatedRow => {
+                                                if(updatedRow){
+                                                    res.status(204).send({});  
+                                                }
+                                            })
+                                            
+                                        }
+                                    })
+                            }
+                        }else{
+                            res.status(401).send({"message" : "401 Unauthorized"});
+                        }
+                    })
+                }
+            })
+        }
+    }
 
 });
 
