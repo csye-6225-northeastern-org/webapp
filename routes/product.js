43b5ba95be65e9ec94a43dd315fe3a3927c74c9d
icecube-pixel <nithinbharadwajd@outlook.com>
2023-02-17 17:42:15 -0500

    Added changes in the code related to Middleware

--- routes/product.js
+++ routes/product.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const authMiddleware = require("../middleware/authMiddleware");
+const validationMiddleware = require("../middleware/validationMiddleware");
 
 const productController = require("../controllers/product");
 
-router.get("/:id", productController.getProductInfo);
+router.get("/:id", [validationMiddleware.validateParams], productController.getProductInfo);
 
 router.put("/:id", productController.putProductInfo);
 
