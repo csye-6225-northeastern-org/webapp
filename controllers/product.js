c971675065d27c846c4cc5832638a1f6cfa5c962
icecube-pixel <nithinbharadwajd@outlook.com>
2023-02-17 19:49:50 -0500

    Made changes related to controller middleware and validator

--- controllers/product.js
+++ controllers/product.js
@@ -125,151 +125,212 @@ exports.postProductInfo = ((req, res) => {
         });
 });
 
+// exports.putProductInfo = ((req, res) => {
+//     const { id } = req.params;
+//     const invalidRequestBody = JSON.stringify(req.body)==="{}";
+//     const {name, description, sku, manufacturer, quantity, date_added, date_last_updated, owner_user_id} = req.body;
+//     const inputCheckBool = checkIdInput(req, res);
+//     if(inputCheckBool){
+//         res.status(400).send({"message" : "400 Bad Request - Invalid Id in the request"});
+//     }else if(invalidRequestBody){
+//         res.status(400).send({"message" : "400 Bad Request - Empty payload sent"});
+//     }
+//     else if(date_added || date_last_updated || owner_user_id){
+//         res.status(400).send({"message" : "400 Bad Request - Cannot send date_added / date_last_updated /owner_user_id"});
+//     }else if(!name || !description || !sku || !manufacturer || !quantity){
+//         res.status(400).send({"message" : "400 Bad Request - All the mandatory fields need to be set"});
+//     }else if(!validations.validateQuantity(quantity)){
+//         res.status(400).send({"message" : "400 Bad Request - Invalid Quantity Sent in the payload"});
+//     }else{
+//         const credentials = checkAuthHeaders(req, res);
+//         if(utils.isObjEmpty(credentials)){
+//             res.status(401).send({"message" : "Unauthorized - No Authorization found in headers"});
+//         }else if(credentials.username==='' || credentials.password === ''){
+//             res.status(401).send({"message" : "Unauthorized - Missing username/password"});
+//         }else{
+//             userService.findOneByUsername(credentials.username)
+//             .then( result =>{
+//                 if(!result){
+//                     res.status(401).send({"message" : "401 Unauthorized. No user found"});
+//                 }else{
+//                     authUtils.comparePassword(credentials.password, result.dataValues.password)
+//                     .then(cmpResult => {
+//                         if(cmpResult){
+//                             const userIdAccessing = result.getDataValue("id");
+//                             productService.findOne(id)
+//                             .then(productRow => {
+//                                 if(!productRow){
+//                                     res.status(400).send({"message" : "400 Not Found"});
+//                                 }else if(productRow.dataValues.owner_user_id !==  userIdAccessing){
+//                                     res.status(403).send({"message" : "403 Forbidden - Not allowed"});
+//                                 }else{
+//                                     productService.findOneBySku(sku)
+//                                     .then(rowFound=>{
+//                                         if(rowFound && parseInt(rowFound.dataValues.id) !== parseInt(id)){
+//                                             res.status(400).send({"message" : "400 Bad Request - SKU Already exists"})
+//                                         }else{
+//                                             productService.updateProductInfo({name, description,
+//                                                 sku, manufacturer, quantity,
+//                                                     date_last_updated : new Date()}, id)
+//                                                 .then( updatedRow => {
+//                                                     if(updatedRow){
+//                                                         res.status(204).send({});  
+//                                                 }
+//                                             })
+//                                         }
+//                                     })
+                                
+//                                 }
+//                             })
+                            
+//                         }else{
+//                             res.status(401).send({"message" : "401 Unauthorized"});
+//                         }
+//                     })
+//                 }
+//             })
+//         }
+//     }
+
+// });
+
 exports.putProductInfo = ((req, res) => {
     const { id } = req.params;
-    const invalidRequestBody = JSON.stringify(req.body)==="{}";
-    const {name, description, sku, manufacturer, quantity, date_added, date_last_updated, owner_user_id} = req.body;
-    const inputCheckBool = checkIdInput(req, res);
-    if(inputCheckBool){
-        res.status(400).send({"message" : "400 Bad Request - Invalid Id in the request"});
-    }else if(invalidRequestBody){
-        res.status(400).send({"message" : "400 Bad Request - Empty payload sent"});
-    }
-    else if(date_added || date_last_updated || owner_user_id){
-        res.status(400).send({"message" : "400 Bad Request - Cannot send date_added / date_last_updated /owner_user_id"});
-    }else if(!name || !description || !sku || !manufacturer || !quantity){
-        res.status(400).send({"message" : "400 Bad Request - All the mandatory fields need to be set"});
-    }else if(!validations.validateQuantity(quantity)){
-        res.status(400).send({"message" : "400 Bad Request - Invalid Quantity Sent in the payload"});
-    }else{
-        const credentials = checkAuthHeaders(req, res);
-        if(utils.isObjEmpty(credentials)){
-            res.status(401).send({"message" : "Unauthorized - No Authorization found in headers"});
-        }else if(credentials.username==='' || credentials.password === ''){
-            res.status(401).send({"message" : "Unauthorized - Missing username/password"});
+    const {name, description, sku, manufacturer, quantity} = req.body;    
+    productService.findOneBySku(sku)
+    .then(rowFound=>{
+        if(rowFound && parseInt(rowFound.dataValues.id) !== parseInt(id)){
+        res.status(400).send({"message" : "400 Bad Request - SKU Already exists"})
         }else{
-            userService.findOneByUsername(credentials.username)
-            .then( result =>{
-                if(!result){
-                    res.status(401).send({"message" : "401 Unauthorized. No user found"});
-                }else{
-                    authUtils.comparePassword(credentials.password, result.dataValues.password)
-                    .then(cmpResult => {
-                        if(cmpResult){
-                            const userIdAccessing = result.getDataValue("id");
-                            productService.findOne(id)
-                            .then(productRow => {
-                                if(!productRow){
-                                    res.status(400).send({"message" : "400 Not Found"});
-                                }else if(productRow.dataValues.owner_user_id !==  userIdAccessing){
-                                    res.status(403).send({"message" : "403 Forbidden - Not allowed"});
-                                }else{
-                                    productService.findOneBySku(sku)
-                                    .then(rowFound=>{
-                                        if(rowFound && parseInt(rowFound.dataValues.id) !== parseInt(id)){
-                                            res.status(400).send({"message" : "400 Bad Request - SKU Already exists"})
-                                        }else{
-                                            productService.updateProductInfo({name, description,
-                                                sku, manufacturer, quantity,
-                                                    date_last_updated : new Date()}, id)
-                                                .then( updatedRow => {
-                                                    if(updatedRow){
-                                                        res.status(204).send({});  
-                                                }
-                                            })
-                                        }
-                                    })
-                                
-                                }
-                            })
-                            
-                        }else{
-                            res.status(401).send({"message" : "401 Unauthorized"});
-                        }
-                    })
+            productService.updateProductInfo({name, description,
+                sku, manufacturer, quantity,
+                    date_last_updated : new Date()}, id)
+                .then( updatedRow => {
+                    if(updatedRow){
+                        res.status(204).send({});  
                 }
             })
         }
-    }
-
+    })
+        
+                          
 });
+
+
+// exports.patchProductInfo = ((req, res) => {
+
+//     const { id } = req.params;
+//     const {name, description, sku, manufacturer, quantity, date_added, date_last_updated, owner_user_id} = req.body;
+//     const inputCheckBool = checkIdInput(req, res);
+//     const invalidRequestBody = JSON.stringify(req.body)==="{}";
+//     if(inputCheckBool){
+//         res.status(400).send({"message" : "400 Bad Request - Invalid Id in the request"});
+//     }else if(invalidRequestBody){
+//         res.status(400).send({"message" : "400 Bad Request - Empty Payload Sent"});
+//     }else if(date_added || date_last_updated || owner_user_id){
+//         res.status(400).send({"message" : "400 Bad Request - Cannot send date_added / date_last_updated /owner_user_id"});
+//     }else if(!validations.validInputsForProductForPatch(req.body)){
+//         res.status(400).send({"message" : "400 Bad Request - Invalid Input in payload"});
+//     }else if(!validations.validateQuantity(quantity)){
+//         res.status(400).send({"message" : "400 Bad Request - Invalid Quantity Sent in the payload"});
+//     }else{
+//         const credentials = checkAuthHeaders(req, res);
+//         if(utils.isObjEmpty(credentials)){
+//             res.status(401).send({"message" : "Unauthorized - No Authorization found in headers"});
+//         }else if(credentials.username==='' || credentials.password === ''){
+//             res.status(401).send({"message" : "Unauthorized - Missing username/password"});
+//         }else{
+//             userService.findOneByUsername(credentials.username)
+//             .then( result =>{
+//                 if(!result){
+//                     res.status(401).send({"message" : "401 Unauthorized. No user found"});
+//                 }else{
+//                     authUtils.comparePassword(credentials.password, result.dataValues.password)
+//                     .then(cmpResult => {
+//                         if(cmpResult){
+//                             const userIdAccessing = result.getDataValue("id");
+//                             productService.findOne(id)
+//                             .then(productRow => {
+//                                 if(!productRow){
+//                                     res.status(400).send({"message" : "400 Not Found"});
+//                                 }else if(productRow.dataValues.owner_user_id !==  userIdAccessing){
+//                                     res.status(403).send({"message" : "403 Forbidden - Not allowed"});
+//                                 }else{
+//                                     if(sku){
+//                                         productService.findOneBySku(sku)
+//                                         .then(rowFound=>{
+//                                             if(rowFound && parseInt(rowFound.dataValues.id) !== parseInt(id)){
+//                                                 res.status(400).send({"message" : "400 Bad Request - SKU Already exists"})
+//                                             }else{
+//                                                 productService.updateProductInfo({name, description,
+//                                                     sku, manufacturer, quantity,
+//                                                         date_last_updated : new Date()}, id)
+//                                                     .then( updatedRow => {
+//                                                         if(updatedRow){
+//                                                             res.status(204).send({});  
+//                                                     }
+//                                                 })
+//                                             }
+//                                         })
+//                                     }else{
+//                                         productService.updateProductInfo({name, description,
+//                                             sku, manufacturer, quantity,
+//                                                 date_last_updated : new Date()}, id)
+//                                             .then( updatedRow => {
+//                                                 if(updatedRow){
+//                                                     res.status(204).send({});  
+//                                             }
+//                                         })
+//                                     }  
+//                                 }
+//                             })
+                            
+//                         }else{
+//                             res.status(401).send({"message" : "401 Unauthorized"});
+//                         }
+//                     })
+//                 }
+//             })
+//         }
+//     }
+// });
+
 
 exports.patchProductInfo = ((req, res) => {
 
-    const { id } = req.params;
-    const {name, description, sku, manufacturer, quantity, date_added, date_last_updated, owner_user_id} = req.body;
-    const inputCheckBool = checkIdInput(req, res);
-    const invalidRequestBody = JSON.stringify(req.body)==="{}";
-    if(inputCheckBool){
-        res.status(400).send({"message" : "400 Bad Request - Invalid Id in the request"});
-    }else if(invalidRequestBody){
-        res.status(400).send({"message" : "400 Bad Request - Empty Payload Sent"});
-    }else if(date_added || date_last_updated || owner_user_id){
-        res.status(400).send({"message" : "400 Bad Request - Cannot send date_added / date_last_updated /owner_user_id"});
-    }else if(!validations.validInputsForProductForPatch(req.body)){
-        res.status(400).send({"message" : "400 Bad Request - Invalid Input in payload"});
-    }else if(!validations.validateQuantity(quantity)){
-        res.status(400).send({"message" : "400 Bad Request - Invalid Quantity Sent in the payload"});
+    const {name, description, sku, manufacturer, quantity} = req.body;
+    const productFields = { 
+        ...(name && { name }), 
+        ...(description && { description }),
+        ...(sku && { sku }),
+        ...(manufacturer && { manufacturer }),
+        ...(quantity && { quantity }),
+        date_last_updated : new Date()
+    };
+
+    if(sku){
+        productService.findOneBySku(sku)
+        .then(rowFound=>{
+            if(rowFound && parseInt(rowFound.dataValues.id) !== parseInt(id)){
+                res.status(400).send({"message" : "400 Bad Request - SKU Already exists"})
+            }else{
+                productService.updateProductInfo(productFields, id)
+                    .then( updatedRow => {
+                        if(updatedRow){
+                            res.status(204).send({});  
+                    }
+                })
+            }
+        })
     }else{
-        const credentials = checkAuthHeaders(req, res);
-        if(utils.isObjEmpty(credentials)){
-            res.status(401).send({"message" : "Unauthorized - No Authorization found in headers"});
-        }else if(credentials.username==='' || credentials.password === ''){
-            res.status(401).send({"message" : "Unauthorized - Missing username/password"});
-        }else{
-            userService.findOneByUsername(credentials.username)
-            .then( result =>{
-                if(!result){
-                    res.status(401).send({"message" : "401 Unauthorized. No user found"});
-                }else{
-                    authUtils.comparePassword(credentials.password, result.dataValues.password)
-                    .then(cmpResult => {
-                        if(cmpResult){
-                            const userIdAccessing = result.getDataValue("id");
-                            productService.findOne(id)
-                            .then(productRow => {
-                                if(!productRow){
-                                    res.status(400).send({"message" : "400 Not Found"});
-                                }else if(productRow.dataValues.owner_user_id !==  userIdAccessing){
-                                    res.status(403).send({"message" : "403 Forbidden - Not allowed"});
-                                }else{
-                                    if(sku){
-                                        productService.findOneBySku(sku)
-                                        .then(rowFound=>{
-                                            if(rowFound && parseInt(rowFound.dataValues.id) !== parseInt(id)){
-                                                res.status(400).send({"message" : "400 Bad Request - SKU Already exists"})
-                                            }else{
-                                                productService.updateProductInfo({name, description,
-                                                    sku, manufacturer, quantity,
-                                                        date_last_updated : new Date()}, id)
-                                                    .then( updatedRow => {
-                                                        if(updatedRow){
-                                                            res.status(204).send({});  
-                                                    }
-                                                })
-                                            }
-                                        })
-                                    }else{
-                                        productService.updateProductInfo({name, description,
-                                            sku, manufacturer, quantity,
-                                                date_last_updated : new Date()}, id)
-                                            .then( updatedRow => {
-                                                if(updatedRow){
-                                                    res.status(204).send({});  
-                                            }
-                                        })
-                                    }  
-                                }
-                            })
-                            
-                        }else{
-                            res.status(401).send({"message" : "401 Unauthorized"});
-                        }
-                    })
-                }
-            })
-        }
-    }
+        productService.updateProductInfo(productFields, id)
+            .then( updatedRow => {
+                if(updatedRow){
+                    res.status(204).send({});  
+            }
+        })
+    }  
+    
 });
 
 // exports.deleteProductInfo = ((req, res) => {
