eb0c9fef8fccdcb414898cf5916ba5a2e512681b
icecube-pixel <dornipadu.n@northeastern.edu>
2023-02-28 22:41:46 -0500

    Added changes by adding middleware

--- services/imageService.js
+++ services/imageService.js
@@ -1,60 +1,37 @@
-const AWS = require('aws-sdk');
-const multer = require('multer');
-const uuid = require('uuid');
-const Image = require('../models/Image');
+const AWS = require("aws-sdk");
+const multer = require("multer");
+const uuid = require("uuid");
+const Image = require("../models/Image");
+const upload = multer({ dest: "uploads/" });
 
-const s3 = new AWS.S3();
+class ImageService {
+  constructor() {
+    this.s3 = new AWS.S3();
+    this.bucketName = process.env.S3_BUCKET_NAME;
+  }
 
-class ImageService{
+  async uploadImage(req, res, next) {
+    try {
+      const image = req.file.buffer;
+      const filename = req.file.originalname;
 
-    constructor(){
-        this.bucketName = process.env.S3_BUCKET_NAME;
-        this.s3 = new AWS.S3();
-        this.upload = multer({
-            storage: multer.memoryStorage(),
-            // limits: { fileSize: 5 * 1024 * 1024 }, // 5MB file size limit
-            fileFilter: (req, file, callback) => {
-              if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-                return callback(new Error('Only image files are allowed!'), false);
-              }
-              callback(null, true);
-            },
-        }).single('image');
+      const uploadParams = {
+        Bucket: this.bucketName,
+        Key: filename,
+        Body: image,
+        ACL: "public-read", // Make the uploaded file public
+        ContentType: req.file.mimetype, // Set the content type of the file
+      };
+
+      // Upload the file to S3
+      return await this.s3.upload(uploadParams).promise();
+
+      // res.status(200).send({ message: "File uploaded successfully" });
+    } catch (error) {
+      return error;
+      // res.status(500).send({ error: "Failed to upload file" });
     }
-
-    async uploadImage(req, res, next) {
-        try {
-          await new Promise((resolve, reject) => {
-            this.upload(req, res, (err) => {
-              if (err) {
-                reject(err);
-              } else {
-                resolve();
-              }
-            });
-          });
-    
-          const file = req.file;
-          if (!file) {
-            throw new Error('No file uploaded!');
-          }
-    
-          const params = {
-            Bucket: this.bucketName,
-            Key: file.originalname,
-            Body: file.buffer,
-            ContentType: file.mimetype,
-            ACL: 'public-read',
-          };
-    
-          const result = await this.s3.upload(params).promise();
-          res.send(`File uploaded successfully: ${result.Location}`);
-        } catch (err) {
-          next(err);
-        }
-      }
-    
+  }
 }
-
 
 module.exports = ImageService;
