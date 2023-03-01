85bb75cfc3622f8814a320129fbcc121a2e89d4c
icecube-pixel <dornipadu.n@northeastern.edu>
2023-02-28 23:31:55 -0500

    Added changes related to middleware upload validation and controller

--- controllers/image.js
+++ controllers/image.js
@@ -9,6 +9,8 @@ exports.uploadProductImage = (req, res, next) => {
   console.log("Inside upload Product Image API ");
   const product_id = req.params.product_id;
   console.log("Inside Upload Product Image API with product-id : ", product_id);
+  const description = req.file.description;
+  console.log("Description : ", description);
   // imageService.uploadImage(req, res, next);
   // res.status(201).send({});
   const file = req.file;
--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -1,5 +1,33 @@
 const multer = require("multer");
+const path = require("path");
 
-const upload = multer({ dest: "uploads/" }).single("image");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    const fileTypes = /jpeg|jpg|png/;
+    const extname = fileTypes.test(
+      path.extname(file.originalname).toLowerCase()
+    );
+    const mimetype = fileTypes.test(file.mimetype);
+    if (mimetype && extname) {
+      cb(null, Date.now() + path.extname(file.originalname));
+    } else {
+      cb(new Error("400 - Cannot upload this file"), false);
+    }
+  },
+});
+
+const upload = (req, res, next) => {
+  const multerUpload = multer({ storage: storage }).single("image");
+  multerUpload(req, res, function (err) {
+    if (err) {
+      res.status(400).json({ message: err.message });
+    } else {
+      next();
+    }
+  });
+};
 
 module.exports = upload;
