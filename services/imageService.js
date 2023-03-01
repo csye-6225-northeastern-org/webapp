188b33a6af05aa5f56b409d2920025bae85fd44e
icecube-pixel <dornipadu.n@northeastern.edu>
2023-03-01 00:13:23 -0500

    Added changes in code to upload to S3

--- services/imageService.js
+++ services/imageService.js
@@ -1,36 +1,29 @@
+require("dotenv").config;
 const AWS = require("aws-sdk");
 const multer = require("multer");
+const fs = require("fs");
 const uuid = require("uuid");
 const Image = require("../models/Image");
 const upload = multer({ dest: "uploads/" });
 
 class ImageService {
   constructor() {
-    this.s3 = new AWS.S3();
+    this.s3 = new AWS.S3({
+      region: process.env.REGION,
+      accessKeyId: process.env.AWS_ACCESS_KEY,
+      secretAccessKey: process.env.AWS_SECRET_KEY,
+    });
     this.bucketName = process.env.S3_BUCKET_NAME;
   }
 
-  async uploadImage(req, res, next) {
-    try {
-      const image = req.file.buffer;
-      const filename = req.file.originalname;
-
-      const uploadParams = {
-        Bucket: this.bucketName,
-        Key: filename,
-        Body: image,
-        ACL: "public-read", // Make the uploaded file public
-        ContentType: req.file.mimetype, // Set the content type of the file
-      };
-
-      // Upload the file to S3
-      return await this.s3.upload(uploadParams).promise();
-
-      // res.status(200).send({ message: "File uploaded successfully" });
-    } catch (error) {
-      return error;
-      // res.status(500).send({ error: "Failed to upload file" });
-    }
+  async uploadFile(file) {
+    const fileStream = fs.createReadStream(file.path);
+    const uploadParams = {
+      Bucket: this.bucketName,
+      Body: fileStream,
+      Key: file.filename,
+    };
+    return this.s3.upload(uploadParams).promise();
   }
 }
 
