b87480bbf41fc171b71fe01fe0ba4d9888e65bf1
icecube-pixel <dornipadu.n@northeastern.edu>
2023-03-01 22:14:12 -0500

    Made changes in the code related Users controllers

--- controllers/users.js
+++ controllers/users.js
@@ -41,54 +41,6 @@ async function getSingleUserRecordByUsername(username){
     })
 }
 
-// exports.getUserInfo = ((req, res) => {
-//     const inputCheckBool = checkIdInput(req, res);
-//     if(inputCheckBool){
-//         res.status(400).send({"message" : "Invalid Id in the request"});
-//     }else{
-//         const credentials = checkAuthHeaders(req, res);
-//         if(utils.isObjEmpty(credentials)){
-//             res.status(401).send({"message" : "Unauthorized - No Authorization found in headers"});
-//         }else if(credentials.username==='' || credentials.password === ''){
-//             res.status(401).send({"message" : "Unauthorized - Missing username/password"});
-//         }
-//         else{
-//             const recordFromDB = getSingleUserRecordByUsername(credentials.username)
-//             recordFromDB.then(result =>{
-//                 if(!result){
-//                     res.status(401).send({"message" : "401 Unauthorized - No Authorization found"});
-//                 }else{
-//                     // Got a row from DB. Need to check if the same username and password
-//                     if(result.dataValues.userName === credentials.username){
-//                         authUtils.comparePassword(credentials.password, result.dataValues.password)
-//                         .then(passwordCompare =>{
-//                             if(passwordCompare){
-//                                 if(parseInt(result.dataValues.id) !== parseInt(req.params.id)){
-//                                     res.status(403).send({"message" : "403 Forbidden - Id incorrect"});
-//                                 }else{
-//                                     res.status(200).send({
-//                                         "id" : result.getDataValue("id"),
-//                                         "first_name" : result.getDataValue("firstName"),
-//                                         "last_name" : result.getDataValue("lastName"),
-//                                         "username" : result.getDataValue("userName"),
-//                                         "account_created" : result.getDataValue("account_created"),
-//                                         "account_updated" : result.getDataValue("account_updated")
-//                                     });
-//                                 }
-//                             }else{
-//                                 res.status(401).send({"message" : "401 Unauthorized - Invalid Password"});
-//                             }
-//                         }) 
-//                     }
-//                     else{
-//                         res.status(401).send({"message" : "401 Unauthorized"});
-//                     }
-//                 }
-//             })
-//         }
-//     }
-// });
-
 exports.getUserInfo = ((req, res) => {
     const inputCheckBool = checkIdInput(req, res);
     if(inputCheckBool){
@@ -106,71 +58,6 @@ exports.getUserInfo = ((req, res) => {
     }
 });
 
-// exports.putUserInfo = ((req, res) =>{
-//     const inputCheckBool = checkIdInput(req, res);
-//     const {id} = req.params;
-//     if(inputCheckBool){
-//         res.status(400).send({"message" : "Invalid Id in the request"});
-//     }else{
-//         const {first_name, last_name, password, username, account_created, account_updated} = req.body;
-
-//         if(username || account_created || account_updated){
-//             res.status(400).send({"message" : "400 Bad Request. Cannot update username / account_created / account_updated "});
-//         }else if(validations.checkEmptyInput(password)){
-//             res.status(400).send({"message" : "400 Bad Request. Empty password sent"});
-//         }
-//         else{
-//             const credentials = checkAuthHeaders(req, res);
-//             if(utils.isObjEmpty(credentials)){
-//                 res.status(401).send({"message" : "Unauthorized - No Authorization found in headers"});
-//             }else if(credentials.username==='' || credentials.password === ''){
-//                 res.status(401).send({"message" : "Unauthorized - Missing username/password"});
-//             }else{
-//                 const recordFromDB = getSingleUserRecordByUsername(credentials.username);
-//                 recordFromDB.then(result =>{
-//                     if(!result){
-//                         res.status(401).send({"message" : "401 Unauthorized - No Authorization found"});
-//                     }else{
-//                         // Got a row from DB. Need to check if the same username and password
-//                         if(result.dataValues.userName === credentials.username){
-//                             authUtils.comparePassword(credentials.password, result.dataValues.password)
-//                             .then( cmpResult => {
-//                                 if(cmpResult){
-//                                     if(parseInt(result.dataValues.id) !== parseInt(req.params.id)){
-//                                         res.status(403).send({"message" : "403 Forbidden"});
-//                                     }else{
-//                                         authUtils.generateHash(credentials.password)
-//                                         .then( hash =>{
-//                                             User.update({
-//                                                     firstName: first_name,
-//                                                     lastName: last_name,
-//                                                     password: hash,
-//                                                     account_updated : new Date()
-//                                                 },{
-//                                                     where : {id}
-//                                             })
-//                                             .then( result => {res.status(204).send({}); })
-//                                             .catch((error) => {res.status(403).send({"message" : "403 Forbidden"}); })
-//                                         })
-//                                     }    
-//                                 }
-//                                 else{
-//                                     res.status(401).send({"message" : "401 Unauthorized"});
-//                                 }
-//                             })
-//                             .catch( error => {
-//                                 res.status(401).send({"message" : "401 Unauthorized", error});
-//                             })
-//                         }else{
-//                             res.status(401).send({"message" : "401 Unauthorized"});
-//                         }
-//                     }
-                    
-//                 })
-//             }
-//         }
-//     }
-// });
 
 exports.putUserInfo = ((req, res) =>{
     const {id} = req.params;
@@ -209,47 +96,6 @@ exports.putUserInfo = ((req, res) =>{
         })
     }    
 });
-
-// exports.postUserInfo = ((req, res) =>{
-//     const {first_name, last_name, password, username, account_created, account_updated} = req.body;
-//     if(account_created || account_updated){
-//         res.status(400).send({"message" : "400 Bad Request. Cannot send account_created / account_updated "});
-//     }else{
-//         const userRecord = getSingleUserRecordByUsername(username);
-//         userRecord.then(record => {
-//             if(record || !validations.validateEmail(username) || validations.checkEmptyInput(first_name) ||
-//             validations.checkEmptyInput(last_name) || validations.checkEmptyInput(password)){
-//                 res.status(400).send({"message" : "400 Bad Request. Invalid payload"});  
-//             }else{
-//                 authUtils.generateHash(password)
-//                 .then(hash => {
-//                     User.create({
-//                         firstName: first_name,
-//                         lastName: last_name,
-//                         password: hash,
-//                         userName: username,
-//                         account_created : new Date(),
-//                         account_updated : new Date() 
-//                     }).then( result =>{
-//                         // console.log("Result : ", result);
-//                         res.status(201).send(
-//                             {
-//                                 "id" : result.getDataValue("id"),
-//                                 "first_name" : result.getDataValue("firstName"),
-//                                 "last_name" : result.getDataValue("lastName"),
-//                                 "username" : result.getDataValue("userName"),
-//                                 "account_created" : result.getDataValue("account_created"),
-//                                 "account_updated" : result.getDataValue("account_updated")
-//                         });
-//                     })
-//                     .catch((error) => {
-//                         res.status(400).send({"message" : "400 Bad Request", error});
-//                     })
-//                 })
-//             }
-//         })
-//     }
-// });
 
 exports.postUserInfo = ((req, res) =>{
     const {first_name, last_name, password, username} = req.body;
