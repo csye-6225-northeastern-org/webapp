9ec76992ad6ff98370f340af9f9c3515e2047ba8
icecube-pixel <dornipadu.n@northeastern.edu>
2023-03-03 14:59:34 -0500

    Added changes in DB file

--- postgres/db.js
+++ postgres/db.js
@@ -1,36 +1,31 @@
-const { Sequelize, DataTypes} = require('sequelize');
-const { dbName,
-        hostUrl, 
-        userName,
-        password,
-        nodeEnv,
-        dbPoolConfig} = require("./config")
+const { Sequelize, DataTypes } = require("sequelize");
+const {
+  dbName,
+  hostUrl,
+  userName,
+  password,
+  nodeEnv,
+  dbPoolConfig,
+} = require("./config");
 
-const sequelize = new Sequelize(dbName, userName, password, 
-        {
-            host: hostUrl,
-            dialect: "postgres",
-            pool : dbPoolConfig,
-            dialectOptions: {
-                ssl: "Amazon RDS",
-              },
-        });
+const sequelize = new Sequelize(dbName, userName, password, {
+  host: hostUrl,
+  dialect: "postgres",
+  pool: dbPoolConfig,
+  // dialectOptions: {
+  //     ssl: "Amazon RDS",
+  //   },
+});
 
-
-if(nodeEnv === 'development'){
-    (
-        async() => {
-            await sequelize.sync({ 
-                force: true 
-            });    
-        }
-    )();
+if (nodeEnv === "development") {
+  (async () => {
+    await sequelize.sync({
+      force: true,
+    });
+  })();
 }
 
-
 module.exports = {
-    sequelize,
-    DataTypes
+  sequelize,
+  DataTypes,
 };
-
-  
