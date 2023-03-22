57710ae69f17e15a3f27715746f5d17be312ee63
icecube-pixel <dornipadu.n@northeastern.edu>
2023-03-22 00:25:54 -0400

    Added changes to logger file

--- utils/logger.js
+++ utils/logger.js
@@ -1,7 +1,24 @@
+var appRoot = require('app-root-path');
 const winston = require('winston');
 
+var config = {
+  file: {
+    level: 'info',
+    filename: `${appRoot}/logs/combined.log`,
+    handleExceptions: true,
+    json: true,
+    maxFiles: 5,
+    colorize: false,
+  },
+  console: {
+    level: 'debug',
+    handleExceptions: true,
+    json: false,
+    colorize: true,
+  },
+};
+
 const logger = winston.createLogger({
-  level: 'info',
   format: winston.format.combine(
     winston.format.timestamp(),
     winston.format.printf(({ timestamp, level, message }) => {
@@ -9,9 +26,17 @@ const logger = winston.createLogger({
     })
   ),
   transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: 'app.log' }),
+    new winston.transports.Console(config.console),
+    new winston.transports.File(config.file),
   ],
+  exitOnError: false
 });
+
+logger.stream = {
+  write: function(message, encoding) {
+    // use the 'info' log level so the output will be picked up by both transports (file and console)
+    logger.info(message);
+  },
+};
 
 module.exports = logger;
