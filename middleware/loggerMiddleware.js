048844b7a8509df70d011c6c3078e83c96454d24
icecube-pixel <dornipadu.n@northeastern.edu>
2023-03-22 00:25:54 -0400

    Added changes to logger file

--- middleware/loggerMiddleware.js
+++ middleware/loggerMiddleware.js
@@ -1,7 +1,18 @@
+const morgan = require('morgan');
+const logger = require('../utils/logger');
+
+// const loggerMiddleware = morgan('combined', { stream: logger.stream });
+
 const loggerMiddleware = (req, res, next) => {
-  const { method, url } = req;
-  const timestamp = new Date().toISOString();
-  logger.info(`${method} ${url} ${timestamp}`);
+  const { method, url, httpVersion } = req;
+  const remoteAddress = req.connection.remoteAddress;
+
+  res.on('finish', () => {
+    const statusCode = res.statusCode;
+    const contentLength = res.get('Content-Length') || '-';
+    logger.info('Request processed', { method, url, httpVersion, remoteAddress, statusCode, contentLength });
+  });
+
   next();
 };
   
