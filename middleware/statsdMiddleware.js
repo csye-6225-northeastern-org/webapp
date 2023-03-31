5aea38c991229b6ecef34ac895a0642cfa34adc6
icecube-pixel <dornipadu.n@northeastern.edu>
2023-03-31 11:35:35 -0400

    Updated the changes to have the method Name in the metrics

--- middleware/statsdMiddleware.js
+++ middleware/statsdMiddleware.js
@@ -3,8 +3,9 @@ const StatsD = require('hot-shots');
 const statsd = new StatsD({ host: 'localhost', port: 8125 });
 
 async function statsMiddleware(req, res, next){
+    const method = req.method;
     const route = req.originalUrl.replace(/\//g, '.').replace(/(\?.*)/g, '');
-    const metricName = `api${route}.calls`;
+    const metricName = `api.${method}${route}.calls`;
     statsd.increment(metricName);
     next();
 };
