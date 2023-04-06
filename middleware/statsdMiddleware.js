b59c9b617eb9618f3e5d68c5a19cf9ae28d3cf64
icecube-pixel <dornipadu.n@northeastern.edu>
2023-04-05 21:20:16 -0400

    Added changes in the code to change the log group name and statsd metric name

--- middleware/statsdMiddleware.js
+++ middleware/statsdMiddleware.js
@@ -1,11 +1,11 @@
 const StatsD = require('hot-shots');
+const logger = require("../utils/logger");
 
 const statsd = new StatsD({ host: 'localhost', port: 8125 });
 
 async function statsMiddleware(req, res, next){
     const method = req.method;
-    // Remove dynamic path parameters
-    const path = req.route.path.replace(/\/:[a-zA-Z]+/g, '');
+    const path = req.originalUrl.replace(/\/:[a-zA-Z]+/g, '').replace(/\/\d+/g, '');
     const route = path.replace(/\//g, '.').replace(/(\?.*)/g, '');
     const metricName = `api.${method}${route}.calls`;
     statsd.increment(metricName);
